25eee8eb76b86b0d34e04654295554676670e40d
Alexandre Girard <alx.girard@gmail.com>
2010-01-28 13:37:15 +0100

    simplify registration

--- barbabot.rb
+++ barbabot.rb
@@ -14,14 +14,20 @@ messenger = Jabber::Simple.new(config['account']['email'], config['account']['pa
 
 class User
   include DataMapper::Resource
-  
   property :id, Serial
-  property :im_account, String
+  property :im_name, String
 end
 
-class Channel
-  include DataMapper::Resource
-  
-  property :id, Serial
-  property :name, String
+while true
+  messenger.received_messages do |msg|
+    unless user = User.first(:im_name => msg.from.to_s)
+      User.create(:im_name => msg.from.to_s)
+      messenger.add(msg.from)
+      messenger.deliver(msg.from, "Bienvenue sur barbabot")
+    end
+    User.all do |user|
+       messenger.deliver(user.im_name, "#{msg.from.to_s.split("@").first}: #{msg.body}") if user.im_name != msg.from.to_s
+    end
+  end  
+  sleep 1
 end
