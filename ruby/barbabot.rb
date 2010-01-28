db7ba23b66c12b95bf985ab2da75026bd9e12c41
Alexandre Girard <alx.girard@gmail.com>
2010-01-28 14:42:41 +0100

    use logger

--- ruby/barbabot.rb
+++ ruby/barbabot.rb
@@ -7,7 +7,9 @@ require 'robustthread'
 DataMapper.setup(:default, "sqlite3:///#{Dir.pwd}/barbabot.db")
 
 config = YAML.load_file('barbabot.yaml')
-messenger = Jabber::Simple.new(config['account']['email'], config['account']['password'])
+@messenger = Jabber::Simple.new(config['account']['email'], config['account']['password'])
+
+RobustThread.logger = Logger.new( 'barbabot.log' )
 
 class User
   include DataMapper::Resource
@@ -17,14 +19,14 @@ end
 
 pid = fork do
   RobustThread.loop( :seconds => 1, :label => "Processing messages..." ) do
-    messenger.received_messages do |msg|
+    @messenger.received_messages do |msg|
       unless user = User.first(:im_name => msg.from.to_s)
         User.create(:im_name => msg.from.to_s)
-        messenger.add(msg.from)
-        messenger.deliver(msg.from, "Bienvenue sur barbabot")
+        @messenger.add(msg.from)
+        @messenger.deliver(msg.from, "Bienvenue sur barbabot")
       end
       User.all.each do |user|
-        messenger.deliver(user.im_name, "#{msg.from.to_s.split("@").first}: #{msg.body}") if user.im_name != msg.from.to_s
+        @messenger.deliver(user.im_name, "#{msg.from.to_s.split("@").first}: #{msg.body}") if user.im_name != msg.from.to_s
       end
     end
   end
