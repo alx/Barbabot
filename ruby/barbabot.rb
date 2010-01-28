000a19c1382062a481b938213a8e7789b98bae18
Alexandre Girard <alx.girard@gmail.com>
2010-01-28 21:40:10 +0100

    add commands to bot

--- ruby/barbabot.rb
+++ ruby/barbabot.rb
@@ -12,6 +12,7 @@ class User
   include DataMapper::Resource
   property :id, Serial
   property :im_name, String
+  property :activated, Boolean, :default => true
 end
 
 while true
@@ -19,10 +20,27 @@ while true
     unless user = User.first(:im_name => msg.from.to_s)
       User.create(:im_name => msg.from.to_s)
       @messenger.add(msg.from)
-      @messenger.deliver(msg.from, "Bienvenue sur barbabot")
+      @messenger.deliver(msg.from, "Bienvenue sur barbabot - /help pour la liste des commandes")
     end
-    User.all.each do |user|
-      @messenger.deliver(user.im_name, "#{msg.from.to_s.split("@").first}: #{msg.body}") if user.im_name != msg.from.to_s
+    
+    case msg.body
+    when /^\/help$/i
+      @messenger.deliver(msg.from, "/help Cette aide\n/names Liste des membres actifs\n/up Activer le chat (par defaut)\n/down Désactiver le chat")
+    when /^\/names$/i
+      user.update :activated => true
+      names = "Utilisateurs actifs:\n"
+      User.all(:im_name.not => msg.from.to_s, :activated => true).each{|u| names << "#{user.im_name}\n"}
+      @messenger.deliver(msg.from, names)
+    when /^\/up$/i
+      user.update :activated => true
+      @messenger.deliver(msg.from, "Chat activé")
+    when /^\/down$/i
+      user.update :activated => false
+      @messenger.deliver(msg.from, "Chat désactivé")
+    else
+      User.all(:im_name.not => msg.from.to_s, :activated => true).each do |user|
+        @messenger.deliver(user.im_name, "#{msg.from.to_s.split("@").first}: #{msg.body}")
+      end
     end
   end
   sleep 1
