0520fca71df02df3dcab89ae9c3bb3454e833af2
Alexandre Girard <alx.girard@gmail.com>
2010-01-28 14:44:35 +0100

    back to simple process

--- ruby/barbabot.rb
+++ ruby/barbabot.rb
@@ -2,14 +2,11 @@ require 'rubygems'
 require 'xmpp4r-simple'
 require 'dm-core'
 require 'dm-timestamps'
-require 'robustthread'
 
 DataMapper.setup(:default, "sqlite3:///#{Dir.pwd}/barbabot.db")
 
 config = YAML.load_file('barbabot.yaml')
 @messenger = Jabber::Simple.new(config['account']['email'], config['account']['password'])
-
-RobustThread.logger = Logger.new( 'barbabot.log' )
 
 class User
   include DataMapper::Resource
@@ -17,20 +14,16 @@ class User
   property :im_name, String
 end
 
-pid = fork do
-  RobustThread.loop( :seconds => 1, :label => "Processing messages..." ) do
-    @messenger.received_messages do |msg|
-      unless user = User.first(:im_name => msg.from.to_s)
-        User.create(:im_name => msg.from.to_s)
-        @messenger.add(msg.from)
-        @messenger.deliver(msg.from, "Bienvenue sur barbabot")
-      end
-      User.all.each do |user|
-        @messenger.deliver(user.im_name, "#{msg.from.to_s.split("@").first}: #{msg.body}") if user.im_name != msg.from.to_s
-      end
+while true
+  @messenger.received_messages do |msg|
+    unless user = User.first(:im_name => msg.from.to_s)
+      User.create(:im_name => msg.from.to_s)
+      @messenger.add(msg.from)
+      @messenger.deliver(msg.from, "Bienvenue sur barbabot")
+    end
+    User.all.each do |user|
+      @messenger.deliver(user.im_name, "#{msg.from.to_s.split("@").first}: #{msg.body}") if user.im_name != msg.from.to_s
     end
   end
-  sleep
+  sleep 1
 end
-
-Process.detach pid
