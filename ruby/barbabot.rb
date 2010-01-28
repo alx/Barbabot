e0747aa467b7bc48c8fccd3c28517ebe4f42aa1e
Alexandre Girard <alx.girard@gmail.com>
2010-01-28 22:59:50 +0100

    add connection and messaging with irc

--- ruby/barbabot.rb
+++ ruby/barbabot.rb
@@ -1,12 +1,15 @@
 require 'rubygems'
 require 'xmpp4r-simple'
+
 require 'dm-core'
 require 'dm-timestamps'
+
+gem 'rif'
+require 'rif/bot'
 
 DataMapper.setup(:default, "sqlite3:///#{Dir.pwd}/barbabot.db")
 
 config = YAML.load_file('barbabot.yaml')
-@messenger = Jabber::Simple.new(config['account']['email'], config['account']['password'])
 
 class User
   include DataMapper::Resource
@@ -15,13 +18,50 @@ class User
   property :is_active, Boolean, :default => true
 end
 
+class BarbabotIrc < RIF::Bot
+  
+  def initialize(nick, server, port, name)
+    super
+    @messages = ""
+  end
+  
+  def on_endofmotd(event)
+    join("#tetalab")
+  end
+  
+  def on_message(event)
+    @messages += "<#{event.nick}>: #{event.message}\n"
+  end
+  
+  def deliver(msg)
+    send_message("#tetalab", msg)
+  end
+end
+
+@messenger = Jabber::Simple.new(config['account']['email'], config['account']['password'])
+
+@irc = BarbabotIrc.new("barbabot", "irc.freenode.net", 6667, "Barbabot")
+@irc.connect
+
 while true
+  
+  deliveries = {}
+  
+  unless @irc.messages.empty?
+    User.all(:im_name.not => msg.from.to_s, :is_active => true).each do |user|
+      @messenger.deliver(user.im_name, @irc.messages)
+    end
+    @irc.messages = ""
+  end
+  
   @messenger.received_messages do |msg|
+    # New user has come to say something, register him
     unless user = User.first(:im_name => msg.from.to_s)
       User.create(:im_name => msg.from.to_s)
       @messenger.add(msg.from)
       @messenger.deliver(msg.from, "Bienvenue sur barbabot - /help pour la liste des commandes")
     end
+    
     
     case msg.body
     when /^\/help$/i
@@ -42,6 +82,7 @@ while true
       User.all(:im_name.not => msg.from.to_s, :is_active => true).each do |user|
         @messenger.deliver(user.im_name, "#{msg.from.to_s.split("@").first}: #{msg.body}")
       end
+      @irc.deliver("#{msg.from.to_s.split("/").first}: #{msg.body}")
     end
   end
   sleep 1
